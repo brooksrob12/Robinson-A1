de48314d620eac9c16a5dce3ac0a3e99b128ab57
Brooks Robinson <a30102408@mcla.edu>
2019-02-11 13:26:06 +0000

    correct file

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ function requestHandler(req, res)
     var url = require('url');
     var url_parts = url.parse(req.url, true);
     var query = url_parts.query;
+    var grades = query['grades'];
+    var count = 0;
+    var text="";
+    var word = query['word']
+    var word1 = query['word1'];
+    var word2 = query['word2'];
+
     
     res.writeHead(200, {'Content-Type': 'text/html'});
     
@@ -21,15 +28,44 @@ function requestHandler(req, res)
       console.log("Handling a request");
       console.log(query);
       var sum = 0;
-      var max = Math.max(query);
-      var min = Math.min(query);
-      
-    for(var i = 0; i < query.length; i++) {
-    sum += query[i];
-    }
-      var avg = sum / query.length;
-      res.write('<pre>'+avg+'</pre>');
+      var parse = parseInt(grades);
+      for (var i in query['grades'])
+      {
+        sum = sum + parseInt(query['grades'][i]);
+        var count = 1+count;
+
+      }
+      var avg=sum/count;
+      const max = Math.max(...grades);
+      const min = Math.min(...grades);
+    
+      res.write('<pre>'+"Ave: "+avg+" Max: "+max+" Min: "+min+'</pre>');
+
       res.end('');
+    }
+        if (query['cmd'] == 'repeat')
+    {
+      console.log("Handling a request");
+      console.log(query);
+      for (i = 0,  text = ""; i < word.length; i++) 
+      { 
+  text += word + "<br>";
+}
+    
+      res.write('<pre>'+text+'</pre>');
+
+      res.end('');
+    }
+      if (query['cmd'] == 'dotted')
+    {
+      console.log("Handling a request");
+      console.log(query);
+      var dotsize= 30-(word1.length + word2.length);
+      var dots = '';
+      for (var i = 0 ; i < dotsize; i++)
+      dots = dots+'.';
+      res.write('<pre>' +word1+ dots +word2+ '</pre>');
+        res.end('');
     }
     else
     {
